fddc93cfcf4c1ed2628c0474ae8e2f0ebda1893e
talis-fb <talisonf11@gmail.com>
2022-05-07 13:18:40 -0300

    Hover with link of Source Code and Site with deploy

--- components/ProjectContainer.vue
+++ components/ProjectContainer.vue
@@ -1,49 +1,78 @@
 <script>
 export default {
   name: 'ProjectContainer',
-  props: ['img', 'title', 'description', 'tools'],
+  props: ['img', 'title', 'description', 'tools', 'sourceCode', 'site'],
   methods: {
-    firstLetterUpperCase(str){
+    firstLetterUpperCase(str) {
       return str.charAt(0).toUpperCase() + str.slice(1)
-    }
-  }
+    },
+  },
 }
 </script>
 
 <template>
-  <v-card id="project-card" class="mx-auto" max-width="344" color="secondary">
-    <v-img :src="img" height="200px">
-      <template v-slot:placeholder>
-        <v-row class="fill-height ma-0" align="center" justify="center">
-          <v-progress-circular
-            indeterminate
-            color="grey lighten-5"
-          ></v-progress-circular>
-        </v-row>
-      </template>
-    </v-img>
-
-    <v-divider></v-divider>
-
-    <v-card-title>{{ title }}</v-card-title>
-
-    <v-card-text align="left">
-      {{ description }}
-    </v-card-text>
-
-    <v-divider class="my-1"></v-divider>
-
-    <v-card-actions>
-      <div class="d-flex m-5">
-
-        <v-tooltip top v-for="(el, i) in tools" :key="i" >
-          <template v-slot:activator="{ on, attrs }">
-            <v-icon v-bind="attrs" v-on="on" :class="`devicon-${el}-plain`" ></v-icon>
+  <v-hover>
+    <template v-slot:default="{ hover }">
+      <v-card
+        id="project-card"
+        class="mx-auto"
+        max-width="344"
+        color="secondary"
+      >
+        <v-img :src="img" height="200px">
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
           </template>
-          <span>{{ firstLetterUpperCase(el) }}</span>
-        </v-tooltip>
 
-      </div>
-    </v-card-actions>
-  </v-card>
+          <v-fade-transition v-if="sourceCode">
+            <v-overlay v-if="hover" absolute color="#036358">
+              <a v-if="site" :href="site" target="_blank">
+                <v-btn><v-icon>mdi-open-in-new</v-icon>Deploy</v-btn>
+              </a>
+
+              <a v-if="sourceCode" :href="sourceCode" target="_blank">
+                <v-btn><v-icon>mdi-github</v-icon>Código fonte</v-btn>
+              </a>
+            </v-overlay>
+          </v-fade-transition>
+        </v-img>
+
+        <v-divider></v-divider>
+
+        <v-card-title>{{ title }}</v-card-title>
+
+        <v-card-text align="left">
+          {{ description }}
+        </v-card-text>
+
+        <v-divider class="my-1"></v-divider>
+
+        <v-card-actions>
+          <div class="d-flex m-5">
+            <v-tooltip top v-for="(el, i) in tools" :key="i">
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon
+                  v-bind="attrs"
+                  v-on="on"
+                  :class="`devicon-${el}-plain`"
+                ></v-icon>
+              </template>
+              <span>{{ firstLetterUpperCase(el) }}</span>
+            </v-tooltip>
+          </div>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-hover>
 </template>
+
+<style>
+a {
+  text-decoration: none;
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -7,6 +7,8 @@
           :title="el.title"
           :description="el.description"
           :tools="el.tools"
+          :sourceCode="el.sourceCode"
+          :site="el.site"
         ></ProjectContainer>
       </v-col>
     </v-row>
@@ -23,25 +25,31 @@ export default {
           title: 'Este portfolio',
           description: 'Esse mesmo',
           tools: ['nuxtjs', 'vuejs', 'vuetify'],
-          img: 'https://github.com/talis-fb/pokedex-vue/raw/master/screnshoot.png',
+          img: 'racoon.jpg',
+          sourceCode: 'https://github.com/talis-fb/portfolio'
         },
         {
           title: 'Pokedex Vue',
           description: 'Um chat bonitao',
           tools: ['typescript', 'vuejs', 'sass'],
           img: 'https://github.com/talis-fb/pokedex-vue/raw/master/screnshoot.png',
+
+          sourceCode: 'https://github.com/talis-fb/pokedex-vue'
+
         },
         {
           title: 'Pokedex React',
           description: 'Um chat bonitao',
           tools: ['javascript', 'react'],
           img: 'https://github.com/talis-fb/pokedex/raw/master/screenshot.png',
+          sourceCode: 'https://github.com/talis-fb/pokedex'
         },
         {
           title: 'Telegram bot',
           description: 'Um chat bonitao',
           tools: ['typescript', 'heroku'],
           img: 'https://wersm.com/wp-content/uploads/2016/11/wersm-telegram-telegraph.jpg',
+          sourceCode: 'https://github.com/talis-fb/telegram_bot'
         },
         {
           title: 'SnakeGame With One Array',
@@ -49,24 +57,58 @@ export default {
             'Jogo da Cobrinha clássico, em JavaScript e HTML, onde todo o campo, cenário, comida e a cobra são unicamente uma Array.',
           tools: ['javascript', 'html5'],
           img: 'snake_game_print.png',
+          sourceCode: 'https://github.com/talis-fb/SnakeGameWithOneArray',
+          site: 'https://talis-fb.github.io/SnakeGameWithOneArray/'
         },
         {
           title: 'notion-clone',
           description: 'Um chat bonitao',
           tools: ['javascript', 'react', 'redux', 'vite', 'emotion'],
           img: 'notion_clone_print.png',
+          sourceCode: 'https://github.com/talis-fb/notion-clone',
+        },
+        {
+          title: 'Ocma',
+          description: 'Um chat bonitao',
+          tools: ['c'],
+          img: 'https://github.com/talis-fb/ocma/raw/main/screen.png',
+
+          sourceCode: 'https://github.com/talis-fb/ocma'
         },
         {
           title: 'Chat',
           description: 'Um chat bonitao',
-          tools: ['javascript', 'react', 'sass', 'nodejs', 'mongodb'],
+          tools: ['javascript', 'react', 'sass', 'nodejs', 'socketio','mongodb'],
           img: 'https://github.com/talis-fb/Chat/raw/main/screenshot1.png',
+          sourceCode: 'https://github.com/talis-fb/Chat'
+        },
+        {
+          title: 'SQLClient',
+          description: 'A very simple, easy, fast and minilmalist client SQLite for beginner practice the language SQL',
+          tools: ['sqlite', 'javascript', 'html5', 'css3'],
+          img: 'https://github.com/talis-fb/SQLClient/raw/master/screenshot.png',
+          sourceCode: 'https://github.com/talis-fb/SQLClient'
+        },
+        {
+          title: 'Calculadora Com React',
+          description: 'Um modelo de calculadora construído em ReactJS inspirado na Calculadora Nativa do Android.',
+          tools: ['javascript', 'react', 'css'],
+          img: 'https://github.com/talis-fb/CalculadoraSimplesComReact/raw/master/screenshot.png',
+          sourceCode: 'https://github.com/talis-fb/CalculadoraSimplesComReact'
+        },
+        {
+          title: 'PlayerVideoWeb',
+          description: 'Um modelo de player de vídeos para web feito em js',
+          tools: ['javascript', 'html5', 'css3'],
+          img: 'player_video_print.png',
+          sourceCode: 'https://github.com/talis-fb/PlayerVideoWeb'
         },
         {
           title: 'BluExodia',
           description: 'Um chat bonitao',
           tools: ['cplusplus', 'arduino'],
-          img: 'https://github.com/talis-fb/pokedex-vue/raw/master/screnshoot.png',
+          img: 'bluexodia_print.jpg',
+          sourceCode: 'https://github.com/talis-fb/BluExodia'
         },
       ],
     }
