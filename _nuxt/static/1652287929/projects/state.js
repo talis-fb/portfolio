a45d8420674d92967502982e3b84c8e202773388
talis-fb <talis-fb@users.noreply.github.com>
2022-05-15 02:09:19 +0000

    deploy: 5bc208f60fef2347e74901a82e9ded05f511b03f

--- _nuxt/static/1652287929/projects/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1652287929",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
